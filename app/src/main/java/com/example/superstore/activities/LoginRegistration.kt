403ee648d44cbdccfa478cabb42c5cf6a039ed39
Mushfiq <mushfiqurr047@gmail.com>
2023-10-01 22:15:34 +0600

    shop

---
+++ app/src/main/java/com/example/superstore/activities/LoginRegistration.kt
@@ -0,0 +1,12 @@
+package com.example.superstore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.superstore.R
+
+class LoginRegistration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_registration)
+    }
+}
